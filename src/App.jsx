4915fcb95c5ecf0ff93fdf128eb877270b6b29c9
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-14 16:12:35 +0100

    build CountryList component, use reduce method

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
@@ -6,8 +9,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
-import { useEffect, useState } from 'react';
-import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
+import CountryList from './components/CountryList';
 polyfillCountryFlagEmojis();
 
 const BASE_URL = 'http://localhost:9000';
@@ -48,7 +50,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
