1796dbd5d1232684d20e29dec20bc9b0d67afa90
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2024-11-14 10:00:47 +0100

    Feat: Basic layout (#2)
    - add basic starter files
    - Build the App Layout

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,3 @@
+export default function AppNav() {
+  return <nav>App Navigation</nav>;
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,26 @@
+import { Link } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import styles from './Homepage.module.css';
 
 export default function Homepage() {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
-      <h1>Homepage</h1>
-    </div>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          TravelTheWorld keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">
+          Start traveling now!
+        </Link>
+      </section>
+    </main>
   );
 }
