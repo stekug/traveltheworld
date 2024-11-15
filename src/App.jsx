ebb878024e5e123a159739ba747755cf72696abf
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-15 09:13:36 +0100

    add useNavigate to programmatic navigate back

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 polyfillCountryFlagEmojis();
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -56,7 +57,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
