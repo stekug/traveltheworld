9f648624c4db98c6f5a2f8b7afcfa04cd38f1601
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2024-11-15 08:52:20 +0100

    feat: Dynamic Routes (#6)
    - add dynamic city routes
    - read and set query string

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,17 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>map</h1>
+      <p>
+        Postion: ${lat}, ${lng}
+      </p>
+    </div>
+  );
 }
