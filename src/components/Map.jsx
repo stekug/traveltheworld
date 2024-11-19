9791c9d57ececdd0424302e772d9234e985e1e07
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-19 12:31:01 +0100

    remove conditional rendering for use your position button

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -42,11 +42,10 @@ export default function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      {!geolocationPosition && (
-        <Button type="position" onClick={getPosition}>
-          {isLoadingPosition ? 'Loading...' : 'Use your position'}
-        </Button>
-      )}
+      <Button type="position" onClick={getPosition}>
+        {isLoadingPosition ? 'Loading...' : 'Use your position'}
+      </Button>
+
       <MapContainer
         center={mapPosition}
         zoom={6}
