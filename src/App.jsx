e5c1899bf01d7aac7ced35090234604b0b7ec598
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-20 14:14:05 +0100

    add Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Form from './components/Form';
 import { Navigate } from 'react-router-dom';
 import { CitiesProvider } from './contexts/CitiesContexts';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 polyfillCountryFlagEmojis();
 
@@ -27,7 +28,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
